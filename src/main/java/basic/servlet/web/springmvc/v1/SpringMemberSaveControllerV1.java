86a0658dec43111d88b2056142630f7684ae590c
kimdaehyun <zbeld123@gmail.com>
2021-07-04 12:01:37 +0900

    Spring-MVC ControllerV2 컨트롤러 통합

---
+++ src/main/java/basic/servlet/web/springmvc/v1/SpringMemberSaveControllerV1.java
@@ -0,0 +1,30 @@
+package basic.servlet.web.springmvc.v1;
+
+import basic.servlet.domain.member.Member;
+import basic.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+public class SpringMemberSaveControllerV1 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members/save")
+    public ModelAndView process(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("member", member);
+        mv.setViewName("save-result");
+        return mv;
+    }
+}
---
+++ src/main/java/basic/servlet/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,50 @@
+package basic.servlet.web.springmvc.v2;
+
+import basic.servlet.domain.member.Member;
+import basic.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * Controller 통합
+ * class level RequestMapping과 method level RequestMapping을 이용
+ */
+
+@Controller
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request, HttpServletResponse response) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("member", member);
+        mv.setViewName("save-result");
+        return mv;
+    }
+
+    @RequestMapping
+    public ModelAndView list() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members", members);
+        return mv;
+    }
+}
