9c5851d4f855722859ed241b3eb3d09dbfe7bbb0
kimdaehyun <zbeld123@gmail.com>
2021-07-08 11:26:47 +0900

    thymeleaf checkbox

--- form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,19 +3,38 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+
+    /**
+     * Method Level ModelAttribute
+     * 각 매핑마다 중복적으로 model에 넣어줘야 하는 데이터를 공통처리
+     * FormItemController에서는 모든 매핑url에서 아래 리턴되는 값을 model에 담아줌
+     */
+    @ModelAttribute("regions")
+    private Map<String, String> regions() {
+        // 순서보장을 위해 LinkedHashMap 사용
+        Map<String, String> regionsMap = new LinkedHashMap<>();
+        regionsMap.put("SEUOL", "서울");
+        regionsMap.put("BUSAN", "부산");
+        regionsMap.put("JEJU", "제주");
+        return regionsMap;
+    }
 
     @GetMapping
     public String items(Model model) {
@@ -31,13 +50,21 @@ public class FormItemController {
         return "form/item";
     }
 
+    /**
+     * 빈 등록 Form을 랜더링해주는 부분에 빈 Item객체를 model을 이용해 넘겨주었음
+     */
     @GetMapping("/add")
-    public String addForm() {
+    public String addForm(Model model) {
+        model.addAttribute("item", new Item());
+
         return "form/addForm";
     }
 
     @PostMapping("/add")
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+
+        //log.info("item.open = {}", item.getOpen());
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
