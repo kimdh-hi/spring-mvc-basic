ada7e810777324e184668df1b099fa4859812ed9
kimdaehyun <zbeld123@gmail.com>
2021-07-10 11:07:03 +0900

    검증 로직 분리 (Validator)

--- validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -23,6 +23,7 @@ import java.util.List;
 public class ValidationItemControllerV2 {
 
     private final ItemRepository itemRepository;
+    private final ItemValidator itemValidator;
 
     @GetMapping
     public String items(Model model) {
@@ -191,7 +192,7 @@ public class ValidationItemControllerV2 {
      * rejectValue, reject 메서드 사용
      * @ModelAttribute 바로 다음에 BindingResult를 써줘서 BindingResult가 ObjectName을 알 수 있도록 한 것을 이용
      */
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV4(
             @ModelAttribute Item item,
             BindingResult bindingResult,
@@ -216,7 +217,7 @@ public class ValidationItemControllerV2 {
         }
 
         if (item.getQuantity() == null || item.getQuantity() < 10 || item.getQuantity() > 9999) {
-            if (item.getPrice() instanceof Integer)
+            if (item.getQuantity() instanceof Integer)
                 bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
         }
 
@@ -240,6 +241,29 @@ public class ValidationItemControllerV2 {
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /**
+     * 컨트롤러에서 검증로직 분리 (Validator)
+     */
+    @PostMapping("/add")
+    public String addItemV5(
+            @ModelAttribute Item item,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes, Model model) {
+
+        itemValidator.validate(item, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            log.info("bindingResult = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+
         return "redirect:/validation/v2/items/{itemId}";
     }
 
